05620f84a727da884851fdd02cb2d14a05c09f95
이희홍 <goode9394@gmail.com>
2020-01-08 14:32:09 +0900

    희홍 커밋

--- src/main/java/com/mtms/service/ReviewReplyService.java
+++ src/main/java/com/mtms/service/ReviewReplyService.java
@@ -1,5 +1,16 @@
 package com.mtms.service;
 
-public interface ReviewReplyService {
+import java.util.List;
 
+import com.mtms.domain.Criteria;
+import com.mtms.domain.ReplyPageDTO;
+import com.mtms.domain.ReplyVO;
+
+public interface ReviewReplyService {
+	public ReplyPageDTO getListPage(Criteria cri, int reviewNo);
+	public List<ReplyVO> getList(Criteria cri, Long reviewNo);	//댓글 전체 조회
+	public int modify(ReplyVO revo);	//댓글 수정
+	public int remove(int replyNo);	//댓글 삭제
+	public ReplyVO get(int replyNo);	//댓글 조회 
+	public int register(ReplyVO revo);	//댓글 등록
 }
