92becca2584acef978d90f92348a518a05b0ad8a
이희홍 <goode9394@gmail.com>
2020-01-08 16:21:12 +0900

    홍커밋

---
+++ src/main/java/com/mtms/controller/LostController.java
@@ -0,0 +1,50 @@
+package com.mtms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.mtms.domain.Criteria;
+import com.mtms.domain.LostVO;
+import com.mtms.service.LostServiceImpl;
+import com.mtms.service.ReviewReplyServiceImpl;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@AllArgsConstructor
+@RequestMapping("/lost/*")
+public class LostController {
+	private LostServiceImpl lostService;
+	@PostMapping("remove") 
+	 public String remove(int lostNo,RedirectAttributes rttr, @ModelAttribute("cri") Criteria cri) {
+		return null;
+		
+	}
+	@PostMapping("modify")
+	public String modify(LostVO lvo, RedirectAttributes rttr, @ModelAttribute("cri") Criteria cri) {
+		return null;
+		
+	}
+	@GetMapping({"get", "modify"}) 
+	public void get(int lostNo, Model model, @ModelAttribute("cri") Criteria cri) {
+		
+	}
+	@GetMapping("list")
+	public void list(Criteria cri, Model model) {
+		
+	}
+	@PostMapping("register")
+	public String register(LostVO lvo, RedirectAttributes rttr) {
+		return null;
+		
+	}
+
+
+}
--- src/main/java/com/mtms/service/ReviewReplyReportService.java
+++ src/main/java/com/mtms/service/ReviewReplyReportService.java
@@ -1,5 +1,16 @@
 package com.mtms.service;
 
-public interface ReviewReplyReportService {
+import java.util.List;
 
+import com.mtms.domain.Criteria;
+import com.mtms.domain.ReplyReportVO;
+
+public interface ReviewReplyReportService {
+	public int getTotalCount(Criteria cri);
+	public List<ReplyReportVO> getList(Criteria cri);
+	public List<ReplyReportVO> getList();
+	public boolean modify(ReplyReportVO rpvo);
+	public boolean remove(long reportVo);
+	public ReplyReportVO get(int reportNo);
+	public void register(ReplyReportVO rpvo);
 }
