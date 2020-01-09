41e20911214c9d566dbc597691a8136e52ac4c21
ohhanna <hue9404@hanmail.net>
2020-01-09 09:50:22 +0900

    신고부분 RESULT : boolean->int

--- src/main/java/com/mtms/domain/ReplyReportVO.java
+++ src/main/java/com/mtms/domain/ReplyReportVO.java
@@ -10,6 +10,6 @@ public class ReplyReportVO {
 	private String memberId;			// 회원 아이디
 	private int replyNo;				// 댓글 번호
 	private String replyReportContent;	// 댓글신고 내용
-	private boolean replyReportResult;	// 댓글 신고 처리 여부
+	private int replyReportResult;	// 댓글 신고 처리 여부
 	private Date replyReportDate;		// 댓글신고날짜
 }
