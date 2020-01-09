758d004f4b9bfb8328a38cdbb5e07bc860659d63
이희홍 <goode9394@gmail.com>
2020-01-09 09:28:30 +0900

    신고 매퍼부분수정 변수명 잘못기재

--- src/main/java/com/mtms/mapper/ReviewReplyReportMapper.java
+++ src/main/java/com/mtms/mapper/ReviewReplyReportMapper.java
@@ -1,5 +1,16 @@
 package com.mtms.mapper;
 
-public interface ReviewReplyReportMapper {
+import java.util.List;
 
+import com.mtms.domain.Criteria;
+import com.mtms.domain.ReplyReportVO;
+
+public interface ReviewReplyReportMapper {
+	public int getTotalCount(Criteria cri);
+	public List<ReplyReportVO> getListWithPaging(Criteria cri);
+	public void insertSelectKey( ReplyReportVO rpvo);							
+	public void insert(ReplyReportVO rpvo);
+	public int update(ReplyReportVO rpvo);
+	public int delete(int replyReportNo);
+	public ReplyReportVO read(int replyReportNo);
 }
--- src/main/java/com/mtms/mapper/ReviewReportMapper.java
+++ src/main/java/com/mtms/mapper/ReviewReportMapper.java
@@ -5,12 +5,15 @@ import java.util.List;
 import com.mtms.domain.Criteria;
 import com.mtms.domain.ReviewReportVO;
 
+
+
 public interface ReviewReportMapper {
 	public int getTotalCount(Criteria cri);
 	public List<ReviewReportVO> getListWithPaging(Criteria cri);
 	public void insertSelectKey(ReviewReportVO rrvo);							
 	public void insert(ReviewReportVO rrvo);
 	public int update(ReviewReportVO rrvo);
-	public int delete(int reportNo);
-	public ReviewReportVO read(int reportNo);
+	public int delete(int reviewReportNo);
+	public ReviewReportVO read(int reviewReportNo);
+
 }
