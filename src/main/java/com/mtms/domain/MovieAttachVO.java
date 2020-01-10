fc281d9d0fceab1214d400a457052312558762ed
leeyeongjoo <dudwn2026@naver.com>
2020-01-10 16:24:52 +0900

    yy
    yy

--- src/main/java/com/mtms/domain/MovieAttachVO.java
+++ src/main/java/com/mtms/domain/MovieAttachVO.java
@@ -7,6 +7,6 @@ public class MovieAttachVO {
 	private String uuid;			// 영화 uuid
 	private String uploadPath;		// 영화 업로드 경로 들어감
 	private String fileName;		// 영화 실제 파일 이름
-	private String fileType;		// 파일 타입
+//	private String fileType;		// 파일 타입
 	private int movieNo;			// 영화 번호
 }
