97a2c6451a109e6f8816188489a6e55db6b2279a
ywc-412 <57555835+ywc-412@users.noreply.github.com>
2020-01-10 14:39:52 +0900

    member, suggest 수정

--- src/main/java/com/mtms/mapper/SuggestMapper.java
+++ src/main/java/com/mtms/mapper/SuggestMapper.java
@@ -2,11 +2,13 @@ package com.mtms.mapper;
 
 import java.util.List;
 
+import com.mtms.domain.Criteria;
+
 import com.mtms.domain.SuggestVO;
 
 public interface SuggestMapper {
 	// 건의사항 등록
-	public void insertSuggest(SuggestVO svo);
+	public void insertSuggestSelectKey(SuggestVO svo);
 	// 건의사항 조회
 	public List<SuggestVO> getSuggestList();
 	// 건의사항 상세 보기
@@ -15,4 +17,6 @@ public interface SuggestMapper {
 	public int deleteSuggest(int suggestNo);
 	// 건의사항 수정
 	public int updateSuggest(SuggestVO svo);
+	
+	public int getTotalCount(Criteria cri);
 }
